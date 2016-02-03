68fe420485a234e0ac18117810a5fc127d5e5aa0
Howard Yeh <howard@metacircus.com>
2016-02-03 11:18:42 +0800

    can generate just a specific example

--- generate-examples.ts
+++ generate-examples.ts
@@ -7,29 +7,58 @@ import {compile} from "./markdown";
 
 import * as path from "path";
 
+main();
+
 function exitIfError(err: Error) {
-  if(err) {
+  if (err) {
     console.log(err);
     process.exit(1);
   }
 }
 
-glob("./examples/*.md", (err,files: string[]) => {
-  files.forEach(async (file) => {
-    var [r,err] = await os.Open(file);
+async function getExampleFiles(): Promise<string[]> {
+  let files = process.argv.slice(2);
+
+  if (files.length > 0) {
+    return files;
+  }
+
+  return new Promise<string[]>((resolve, reject) => {
+    glob("./examples/*.md", (err, files: string[]) => {
+      if(err) {
+        reject(err);
+        return;
+      }
+
+      resolve(files);
+    });
+  });
+}
+
+async function main() {
+  try {
+    let files = await getExampleFiles();
+    console.log("generate test output for", files);
+    files.forEach(generateOutputForExample);
+  } catch(err) {
     exitIfError(err);
+  }
+}
 
-    var [src,err] = await io.ReadFull(r);
-    exitIfError(err);
+async function generateOutputForExample(file: string) {
+  var [r, err] = await os.Open(file);
+  exitIfError(err);
 
-    var tree = compile(src);
+  var [src, err] = await io.ReadFull(r);
+  exitIfError(err);
 
-    var [w,err] = await os.Create(file + ".json");
-    exitIfError(err);
+  var tree = compile(src);
 
-    w.Write(JSON.stringify(tree,null,2))
+  var [w, err] = await os.Create(file + ".json");
+  exitIfError(err);
 
-    var err = await w.Close();
-    exitIfError(err);
-  })
-});
+  w.Write(JSON.stringify(tree, null, 2))
+
+  var err = await w.Close();
+  exitIfError(err);
+}
